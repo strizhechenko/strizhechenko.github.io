e048203683a8ecb8dc83156d22570ec8316318c0
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-11-24 15:50:34 +0500

    Separate books from skills

--- scripts/skillmap_update.sh
+++ scripts/skillmap_update.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 readonly input=share/my-skills.gv
+readonly books=share/books.gv
 
 for format in png svg; do
 	output=images/my-skills.$format
 	sfdp -Goverlap=prism $input | \
+		gvmap -e | \
+		neato -Ecolor="#55555522" -n2 -T$format > $output
+	if [ "$#" = '0' ]; then
+		git add $output
+	fi
+
+	output=images/books.$format
+	sfdp -Goverlap=prism $books | \
 		gvmap -e | \
 		neato -Ecolor="#55555522" -n2 -T$format > $output
 	if [ "$#" = '0' ]; then
