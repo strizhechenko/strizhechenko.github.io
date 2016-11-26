cfa2d7d593bcda2674d2ab34195b5ec80f7a1958
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-11-26 14:12:12 +0500

    Added(music.svg)

--- scripts/skillmap_update.sh
+++ scripts/skillmap_update.sh
@@ -6,5 +6,6 @@ gv2map share/my-skills.gv images/my-skills.svg
 gv2map share/books.gv images/books.svg
 gv2map share/movies.gv images/movies.svg
 gv2map share/games.gv images/games.svg
+gv2map share/music.gv images/music.svg
 
 [ "$#" != '0' ] || git add images/*.svg
